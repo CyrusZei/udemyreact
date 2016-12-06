3ad0ba89925a3e66a783520d823b9e2c4181b1b6
Cyrus Zei <cyruszei@gmail.com>
2016-12-06 11:18:04 +0100

    refactor app.js from ES6 to javascript

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 import Greeter from './features/greeter';
 
 
-
-class App extends Component {
-
-  render() {
-
-    return (
+var App = React.createClass({
+  render:function(){
+    return(
       <div>
         <Greeter />
       </div>
     );
   }
-}
+});
+
 
 export default App;
