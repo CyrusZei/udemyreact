9b3bb6caeab26aa04d38f8eac91575171f0d15ad
Cyrus Zei <cyruszei@gmail.com>
2016-12-06 09:49:52 +0100

    Hello World

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+import Greeter from './features/greeter';
+
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <Greeter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/features/greeter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+var Greeter = React.createClass({
+  render: function(){
+    return(
+      <div>
+      <h1>  Hello World, from Greeter component</h1>
+      </div>
+    );
+  }
+});
+
+
+
+export default Greeter;
